5c2230c3d68fa7bb64c32286382ba4cace7f5b60
mc <mcapsan@gmail.com>
2024-05-26 01:12:29 -0400

    Create operate function to perform valid calculations

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ function divide(num1, num2) {
 	return num1 / num2;
 }
 
+function operate(num1, operator, num2) {
+	switch (operator) {
+		case "+":
+			return add(num1, num2);
+			break;
+		case "-":
+			return subtract(num1, num2);
+			break;
+		case "*" || "x" || ".":
+			return multiply(num1, num2);
+			break;
+		case "/":
+			return divide(num1, num2);
+			break;
+		default: 
+			break;
+	}
+}
+
