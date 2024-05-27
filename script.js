c2e7e8ab26035baf936eaffec402a42259b02af2
mc <mcapsan@gmail.com>
2024-05-27 09:58:01 -0400

    Fix zero division message showing up for every operation

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function divide(num1, num2) {
 }
 
 function operate(num1, operator, num2) {
-	if (num2 == 0) {
+	if (num2 == 0 && operator === "/") {
 		alert("Erm, What the sigma?");
 		return 69420;
 	}
